3f2780545e40ab99790b0556afc5c9018cc58e9d
Jefferson <jefry04@gmail.com>
2021-01-25 19:43:29 -0500

    add new page

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -6,6 +6,10 @@ export function clearToken() {
   localStorage.removeItem('token');
 }
 
+export function getToken() {
+  return localStorage.getItem('token');
+}
+
 export function isAuthenticaded() {
   const token = localStorage.getItem('token');
   return Boolean(token);
