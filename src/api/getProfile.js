77607cea75ca4af244aaaa192b1d87471fc34d7b
Jefferson <jefry04@gmail.com>
2021-01-29 15:45:40 -0500

    add get profile

---
+++ src/api/getProfile.js
@@ -0,0 +1,22 @@
+import http from './http';
+import * as Auth from '../utils/auth';
+
+export function getProfile(username) {
+  return http
+    .get(`/users/${username}`, {
+      headers: {
+        'x-access-token': Auth.getToken(),
+      },
+    })
+    .then((response) => {
+      const { data = {} } = response;
+
+      const { items = [] } = data;
+      const [item = {}] = items;
+      const { name = '', username = '' } = item;
+      const profile = { name, username };
+
+      console.log(profile);
+      return profile;
+    });
+}
--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,11 @@
 import * as tweets from './tweets';
 import * as users from './users';
+import * as profile from './getProfile';
 
 const API = {
   ...tweets,
   ...users,
+  ...profile,
 };
-
+window.API = API;
 export default API;
