55a6f8fa53d7f53d2ec082135c58cc2c83bcaf22
Jefferson <jefry04@gmail.com>
2021-02-05 22:53:57 -0500

    Add Comment feature

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -2,8 +2,12 @@ import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
 import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
 import Typography from '@material-ui/core/Typography';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import { makeStyles } from '@material-ui/core/styles';
@@ -19,12 +23,25 @@ export default function Tweet({
   content = '',
   date = '',
   user = {},
+  likes = 0,
+  comments = [],
   onSelected,
+  onLiked,
+  onComment,
 }) {
   const classes = useStyles();
 
   function handleClick(e) {
     onSelected && onSelected(e, id);
+  }
+  function handleLiked(e) {
+    e.stopPropagation();
+    onLiked && onLiked(e, id);
+  }
+
+  function handleComment(e) {
+    e.stopPropagation();
+    onComment && onComment(e, id);
   }
 
   return (
@@ -44,6 +61,28 @@ export default function Tweet({
           {content}
         </Typography>
       </CardContent>
+      <CardActions disableSpacing>
+        <IconButton onClick={handleLiked}>
+          {likes ? (
+            <>
+              <Typography variant="subtitle1" gutterBottom>
+                {likes}
+              </Typography>
+              <FavoriteIcon />
+            </>
+          ) : (
+            <FavoriteBorderIcon />
+          )}
+        </IconButton>
+        <IconButton onClick={handleComment}>
+          {comments.length > 0 && (
+            <Typography variant="subtitle1" gutterBottom>
+              {comments.length}
+            </Typography>
+          )}
+          <ChatBubbleOutlineIcon />
+        </IconButton>
+      </CardActions>
     </Card>
   );
 }
--- src/pages/SingleTweet.js
+++ src/pages/SingleTweet.js
@@ -2,20 +2,94 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Tweet from '../components/Tweet';
 import API from '../api';
+import { Button, Paper, TextField, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    margin: `${theme.spacing(2)}px 0`,
+    padding: theme.spacing(2),
+  },
+}));
 
 export default function SingleTweet() {
+  const [showForm, setShowForm] = useState(false);
   const [tweet, setTweet] = useState(null);
   const params = useParams();
   const { id = null } = params;
+  const classes = useStyles();
 
   async function loadTweet({ id }) {
     const data = await API.getTweet({ id });
     setTweet(data);
   }
+  async function onLiked(e, id) {
+    try {
+      await API.likeTweet({ id });
+      // setTweet({
+      //   ...tweet,
+      //   likes: tweet.likes ? tweet.likes + 1 : 1,
+      // });
+      await loadTweet({ id });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  function onComment(e) {
+    setShowForm(true);
+  }
+
+  async function onSubmit(e) {
+    e.preventDefault();
+    const { comment } = e.target.elements;
+    try {
+      await API.newComment({ id, comment: comment.value });
+      await loadTweet({ id });
+      setShowForm(false);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   useEffect(() => {
     loadTweet({ id });
   }, [id]);
-
-  return <>{tweet && <Tweet {...tweet} />}</>;
+  return (
+    <>
+      {tweet && <Tweet {...tweet} onLiked={onLiked} onComment={onComment} />}
+      {showForm && (
+        <form onSubmit={onSubmit}>
+          <Paper className={classes.container}>
+            <TextField
+              label="Comment:"
+              variant="outlined"
+              name="comment"
+              type="textarea"
+              fullWidth
+            />
+          </Paper>
+          <Button variant="contained" color="primary" type="submit">
+            Comment
+          </Button>
+        </form>
+      )}
+      {tweet && tweet.comments.length > 0 && (
+        <>
+          {tweet.comments.map(({ comment, user, _id }) => {
+            return (
+              <Paper className={classes.container} key={_id}>
+                <Typography variant="subtitle2" gutterBottom>
+                  {user.name} @{user.username}
+                </Typography>
+                <Typography variant="subtitle1" gutterBottom>
+                  {comment}
+                </Typography>
+              </Paper>
+            );
+          })}
+        </>
+      )}
+    </>
+  );
 }
